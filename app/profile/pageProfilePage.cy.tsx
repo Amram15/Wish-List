7f18824ffaa7f943d1221ffc6fcd5ef4c13368ae
Amram15 <blessontenson@gmail.com>
2024-04-28 23:40:27 -0400

    Added some negative test cases
    Added negative test cases and functionality test cases for forgot password, home page, and Profile Page
    
    Co-Authored-By: Pollypanda <95537416+Pollypanda@users.noreply.github.com>

--- app/profile/pageProfilePage.cy.tsx
+++ app/profile/pageProfilePage.cy.tsx
@@ -2,17 +2,50 @@ import React from "react";
 import ProfilePage from "./page";
 import "../globals.css";
 
-describe("<ProfilePage />", () => {
-  it("renders", () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<ProfilePage />);
-  });
+describe("Rendering page", () => {
+	it("renders", () => {
+		// see: https://on.cypress.io/mounting-react
+		cy.mount(<ProfilePage />);
+	});
+});
 
-  it("Should allow users to edit their profile", () => {
-    cy.mount(<ProfilePage />);
-    cy.get("button").click();
-    cy.get("#fullName").eq(0).type("NewName");
-    cy.get("#phoneNumber").eq(0).type("1234");
-    cy.get("button").click();
-  });
+describe("Edit Name", () => {
+	it("Should allow users to edit their profile", () => {
+		cy.mount(<ProfilePage />);
+		cy.get("button").click();
+		cy.get("#fullName").eq(0).type("NewName");
+		cy.get("#phoneNumber").eq(0).type("1234");
+		cy.get("button").click();
+		cy.get(".profile-details > :nth-child(1)").contains(
+			new RegExp("NewName", "g")
+		);
+	});
+});
+
+describe("Edit Phone", () => {
+	it("Should allow users to edit their profile", () => {
+		cy.mount(<ProfilePage />);
+		cy.get("button").click();
+		cy.get("#fullName").eq(0).type("NewName");
+		cy.get("#phoneNumber").eq(0).type("1234");
+		cy.get("button").click();
+		cy.get(".profile-details > :nth-child(3)").contains(
+			new RegExp("1234", "g")
+		);
+	});
+});
+
+describe("Edit Phone Should only be numbers", () => {
+	it("Should allow users to edit their profile", () => {
+		cy.mount(<ProfilePage />);
+		cy.get("button").click();
+		cy.get("#fullName").eq(0).type("NewName");
+		cy.get("#phoneNumber").eq(0).type("abcd");
+		cy.get("button").click();
+		cy.get(".profile-details > :nth-child(3)").should((value) => {
+			expect(Number.isInteger(+value), "input should be an integer").to.eq(
+				true
+			); // passes
+		});
+	});
 });
